14bd191425e2b329c010bd14b59ec71202ccf8cb
phuong23401 <hoaphuong23401@gmail.com>
2021-04-22 10:49:55 +0700

    add

---
+++ src/InsertionSort.java
@@ -0,0 +1,21 @@
+public class InsertionSort {
+    static int[] arr = {1, 6, 7, 5, 9, -5, 90, 45};
+
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            int currentElement = list[i];
+            int j;
+            for (j = i - 1; j >= 0 && list[j] > currentElement; j--) {
+                list[j + 1] = list[j];
+            }
+            list[j + 1] = currentElement;
+        }
+    }
+
+    public static void main(String[] args) {
+        insertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
